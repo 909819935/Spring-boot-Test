6e0b2ed741b8ea3603459c61db3f3b025d36175c
dzc <dzc@gjy.com>
2022-06-15 14:54:26 +0800

    Third submit

--- src/main/java/com/gjy/computer/controller/TestController.java
+++ src/main/java/com/gjy/computer/controller/TestController.java
@@ -3,5 +3,6 @@ package com.gjy.computer.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("dzc.........");
+        System.out.println("ly.........");
     }
 }
